4ed61fad0db4201d01fcc5e69fada6fb9af3bc24
Albin Dahlén <albin@dahlen.ws>
2023-09-28 23:31:34 +0200

    Added login to api

--- project/frontend/src/pages/login.js
+++ project/frontend/src/pages/login.js
@@ -1,22 +1,43 @@
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { baseUrl } from "../shared";
 
 export const Login = () => {
-  
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    const response = await fetch(baseUrl + "user/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });
+
+    const data = await response.json();
+    console.log(data);
+  };
 
   return (
     <Container className="d-flex justify-content-center align-items-center vh-100">
       <div className="p-5 rounded bg-light">
         <h2 className="mb-4 text-center">Social App</h2>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Group controlId="username" className="mb-3">
             <Form.Label>Username</Form.Label>
-            <Form.Control type="text" placeholder="Username" size="lg" />
+            <Form.Control type="text" placeholder="Username" onChange={e => setUsername(e.target.value)} size="lg" />
           </Form.Group>
 
           <Form.Group controlId="password" className="mb-4">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" size="lg" />
+            <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} size="lg" />
           </Form.Group>
 
           <div className="d-flex justify-content-center">
@@ -28,7 +49,7 @@ export const Login = () => {
         <hr />
         <Row>
           <Col>
-            <p>
+          <p className="text-center">
               Don't have an account? <Link to="/register">Sign Up</Link>
             </p>
           </Col>
