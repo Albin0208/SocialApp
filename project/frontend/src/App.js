5cde7a4fde7c009c37d9fe3972d10b6494a938a7
Albin Dahlén <albin@dahlen.ws>
2023-10-01 11:42:37 +0200

    Added start for protected routes

--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -1,4 +1,4 @@
-import {  Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Login } from "./pages/Login.js"; // import the Login component
 import { Register } from "./pages/Register.js"; // import the Register component
 import { Home } from "./pages/Home.js"; // import the Home component
@@ -8,21 +8,18 @@ import { ProtectedRoutes } from "./utils/ProtectedRoutes.jsx";
 
 const App = () => {
   return (
-    <>
       <Routes>
-        {/* <Route element={<ProtectedRoutes />}> */}
-          {/* <Route path="/" element={<Home />} /> */}
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<Home />} />
+          </Route>
         </Route>
-        {/* </Route> */}
         <Route path="auth/" element={<AuthLayout />}>
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
         </Route>
         <Route path="*" element={<h1>Not Found!</h1>} />
       </Routes>
-    </>
   );
 };
 export default App;
--- project/frontend/src/components/authLayout.jsx
+++ project/frontend/src/components/authLayout.jsx
@@ -6,13 +6,12 @@ export const AuthLayout = () => {
     <Container className="my-5">
       <Row className="mx-auto w-100">
         <Col className="mx-auto" md={7} lg={5}>
-
-      <div className="p-5 rounded bg-light">
-        <h2 className="mb-4 text-center">Social App</h2>
-        <Outlet />
-      </div>
+          <div className="p-5 rounded bg-light">
+            <h2 className="mb-4 text-center">Social App</h2>
+            <Outlet />
+          </div>
         </Col>
       </Row>
     </Container>
   );
-}
+};
