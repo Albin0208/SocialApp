dbc9b4c54c843ed8d0f44529055369281edcb7ce
Albin Dahlén <albin@dahlen.ws>
2023-10-05 16:34:54 +0200

    Added doc

--- project/backend/controllers/postController.js
+++ project/backend/controllers/postController.js
@@ -1,36 +1,58 @@
-import Post from '../models/postModel.js';
+import Post from "../models/postModel.js";
 
+/**
+ * Retrieves all posts from the database.
+ *
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @returns {Object} - Returns an object containing all posts.
+ */
 export const getPosts = async (req, res) => {
   try {
     const posts = await Post.find();
     res.status(200).json(posts);
   } catch (error) {
     res.status(500).json({
-      message: error.message
+      message: error.message,
     });
   }
-}
+};
 
+/**
+ * Creates a new post with the given content and author.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} req.body - The request body.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A Promise that resolves with the post object or rejects with an error.
+ */
 export const createPost = async (req, res) => {
   try {
     const { content, author } = req.body;
-    const newPost = await Post.create({content, author});
+    const newPost = await Post.create({ content, author });
     res.status(201).json(newPost);
-  }
-  catch (error) {
+  } catch (error) {
     res.status(400).json({
-      message: error.message
+      message: error.message,
     });
   }
-}
+};
 
+/**
+ * Get a post by its ID.
+ * @async
+ * @function
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A Promise that resolves with the post object or rejects with an error.
+ */
 export const getPostById = async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     res.status(200).json(post);
   } catch (error) {
     res.status(404).json({
-      message: error.message
+      message: error.message,
     });
   }
-}
+};
--- project/backend/middleware/verifyJWT.js
+++ project/backend/middleware/verifyJWT.js
@@ -1,6 +1,13 @@
 import jwt from "jsonwebtoken";
 import "dotenv/config";
 
+/**
+ * Middleware function to verify a JSON Web Token (JWT) in the Authorization header of a request.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ * @returns {Object} - Returns a response object with an error message if the token is invalid or missing, otherwise calls the next middleware function.
+ */
 export const verifyJWT = (req, res, next) => {
   const authToken = req.headers["authorization"];
   if (!authToken)
